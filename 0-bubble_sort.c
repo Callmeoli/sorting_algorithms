c5193b3a603e842de110d826b69b7004e9119449
Callmeoli <olikorma@gmail.com>
2022-10-07 08:12:24 +0300

    Hot-fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,11 +9,11 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int tmp;
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		for(j = 0; j < size - 1 - i; j++)
+		for (j = 0; j < size - 1 - i; j++)
 		{
-			if(array[j] > array[j + 1])
+			if (array[j] > array[j + 1])
 			{
 			tmp = array[j];
 			array[j] = array[j + 1];
--- sort.h
+++ sort.h
@@ -18,5 +18,4 @@ int swap_(int *a, int *b);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
-
 #endif
