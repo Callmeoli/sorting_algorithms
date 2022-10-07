2853056b332574ccade6ca30aeccd8499f09198c
Callmeoli <olikorma@gmail.com>
2022-10-07 09:27:33 +0300

    indentiona

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,24 +8,24 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, temp, min_pos;
+	size_t i, j, temp, min_pos;
 
-    for (i = 0; i < size - 1; i ++)
-    {
-        min_pos = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_pos])
-            {
-                min_pos = j;
-            }
-        }
-        if (min_pos != i)
-        {
-            temp = array[i];
-            array[i] = array[min_pos];
-            array[min_pos] = temp;
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size - 1; i ++)
+	{
+		min_pos = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_pos])
+			{
+				min_pos = j;
+			}
+		}
+		if (min_pos != i)
+		{
+			temp = array[i];
+			array[i] = array[min_pos];
+			array[min_pos] = temp;
+			print_array(array, size);
+		}
+	}
 }
