6097ba50a7901ccb693612bb8848ce2ac039c860
Callmeoli <olikorma@gmail.com>
2022-10-07 11:00:30 +0300

    quick_sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+/**
+* quick_sort - sort array using quick sort algo
+* @array: array to be sorted
+* @size: size of the array
+*/
+void quicksort_recursion(int array[], int low, int high, int size);
+int partition(int array[], int low, int high, int size);
+void swap(int *x, int *y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+void quick_sort(int *array, size_t size)
+{
+
+	quicksort_recursion(array, 0, (int)size - 1, (int) size);
+}
+void quicksort_recursion(int array[], int low, int high, int size)
+{
+	 if (low < high)
+	{
+		int pivot_index = partition(array, low, high, (int) size);
+		quicksort_recursion(array, low, pivot_index - 1, (int) size);
+		quicksort_recursion(array, pivot_index + 1, high, (int) size);
+	}
+}
+int partition(int array[], int low, int high, int size)
+{
+	int pivot_value = array[high];  
+	int i = low;
+	int j;
+
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= pivot_value)
+		{
+			swap(&array[i], &array[j]);
+			i++;
+		}
+	}
+swap(&array[i], &array[high]);
+print_array(array, size);
+return i;
+}
