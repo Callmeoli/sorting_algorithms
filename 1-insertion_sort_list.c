9784aa5264772a457c4dffd61c135879200c4b36
Kedir Mohamed <kedir10mohamed45@gmail.com>
2022-10-10 11:08:32 +0300

    fixed betty-style error and replaced for loop with while loop1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,6 @@
 #include "sort.h"
 /**
- * insertion_sort_list - sort list using insertion method
+ * insertion_sort_list - sort list using insertion algorithm
  * @list: list to be sorted
  */
 void insertion_sort_list(listint_t **list)
@@ -8,17 +8,19 @@ void insertion_sort_list(listint_t **list)
 	int j, key;
  	listint_t head;
 
-	for(head = *list->next; head->next != NULL; head = head->next)
+	head = *list->next;
+	while (head->next != NULL)
 	{
-		key = head->n; 
+		key = head->n;
 		j = head->prev;
 
 
 		while(j != NULL && j->n > key)
 		{
-			head->n = j; 
+			head->n = j;
 			j = j->prev;
 		}
 		head->n = key;
+		head = head->next
 	}
 }
