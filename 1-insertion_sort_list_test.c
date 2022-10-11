05a8284051111a37a7fa351193b6d768ecb49431
Kedir Mohamed <kedir10mohamed45@gmail.com>
2022-10-11 10:51:09 +0300

    fixed bug in task1

---
+++ 1-insertion_sort_list_test.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sort list using insertion algorithm
+ * @list: list to be sorted
+ */
+void insertion_sort_list(listint_t **list)
+{
+	int tmp, *p;
+ 	listint_t *head, *j;
+
+	head = *list;
+	while (head->next != NULL)
+	{
+		if (head->n > head->next->n)
+		{
+			p = (int *)&(head->next->n);
+			tmp = head->next->n;
+			*p= (int)head->n;
+			p = (int *)&(head->n);
+			*p = tmp;
+		}
+		j = head;
+		while(j->prev != NULL)
+		{
+			if (j->prev->n > j->n)
+			{
+				tmp =j->n;
+				p = (int *)&(j->n);
+				*p =(int)j->prev->n;
+				p = (int *)&(j->prev->n);
+				*p = tmp;
+			}
+			j = j->prev;
+		}
+		head = head->next;
+	}
+}
